126b278ee976eebe98d18c18250ea05bf8a828b1
Cư Hoàng Ngọc <20520145@gm.uit.edu.vn>
2022-10-25 13:28:20 +0700

    update home page: adding Navbar, Hero, WhyChooseUs components

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,25 @@
 import Head from "next/head";
 import Image from "next/image";
+import Hero from "../components/Hero/Hero";
+import Navbar from "../components/Navbar/Navbar";
+import WhyChooseUs from "../components/WhyChooseUs/WhyChooseUs";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>IE Solution</title>
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
       </Head>
-      <h1>Welcome to our site</h1>
-    </div>
+
+      <Navbar />
+
+      {/* main */}
+      <div className="container mx-auto px-2 md:px-4">
+        <Hero />
+        <WhyChooseUs />
+      </div>
+      {/* footer */}
+    </>
   );
 }

f0e4f1446eed0e9a8c01a5f0ce9f4ff23bb678c6
tunglam241 <20521521@gm.uit.edu.vn>
2022-12-08 14:15:08 +0700

    update footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,8 +3,10 @@ import { AiFillMail } from "react-icons/ai"
 import { BsFillTelephoneFill } from "react-icons/bs"
 import Logo from "../../public/asset/Logo-only.png";
 import Image from "next/image";
+import { useState } from "react";
 
 export const Footer = () => {
+  
   return (
     <div className="bg-[#211e3b] mt-10 pb-10">
       <a href="#" className='flex justify-center items-center gap-2'>
@@ -17,30 +19,30 @@ export const Footer = () => {
           <div className="flex flex-row justify-center">
             <AiOutlineGlobal size={40} className='text-blue-500 my-2'></AiOutlineGlobal>
           </div>
-          <div className="text-white text-center">411 University St, Seattle, USA</div>
-          <div className="text-white text-center">Our Address</div>
+          <div className="text-white text-center "><a className="hover:text-blue-500" href="https://www.google.com/maps/place/Tr%C6%B0%E1%BB%9Dng+%C4%90%E1%BA%A1i+h%E1%BB%8Dc+C%C3%B4ng+ngh%E1%BB%87+Th%C3%B4ng+tin+-+%C4%90HQG+TP.HCM/@10.8700142,106.8008654,17z/data=!3m1!4b1!4m6!3m5!1s0x317527587e9ad5bf:0xafa66f9c8be3c91!8m2!3d10.8700089!4d106.8030541!16s%2Fm%2F02qqlmm?hl=vi">Khu phố 6, P.Linh Trung, Tp.Thủ Đức, Tp.Hồ Chí Minh</a></div>
+          <div className="text-white text-center">Địa chỉ</div>
         </div>
         <div>
           <div className="flex flex-row justify-center">
             <AiFillMail size={40} className='text-blue-500 my-2'></AiFillMail>
           </div>
-          <div className="text-white text-center">contact@oceanthemes.net</div>
-          <div className="text-white text-center">Our Mailbox</div>
+          <div className="text-white text-center"><a className="hover:text-blue-500" href="mailto:info@uit.edu.vn">info@uit.edu.vn</a></div>
+          <div className="text-white text-center">Email</div>
         </div>
         <div>
           <div className="flex flex-row justify-center">
             <BsFillTelephoneFill size={40} className='text-blue-500 my-2'></BsFillTelephoneFill>
           </div>
-          <div className="text-white text-center">+1 -800-456-478-23</div>
-          <div className="text-white text-center">Our Phone</div>
+          <div className="text-white text-center"><a className="hover:text-blue-500" href="tel:0123456789">+84 -0123456789</a></div>
+          <div className="text-white text-center">Điện thoại</div>
         </div>
       </div>
       <div className="hidden md:grid md:grid-cols-5 text-white px-60 text-lg mb-10">
-        <a href="#" className="hover:text-blue-500">Home</a>
-        <a href="#" className="hover:text-blue-500">Services</a>
-        <a href="#" className="hover:text-blue-500">Portfolio</a>
-        <a href="#" className="hover:text-blue-500">Blog</a>
-        <a href="#" className="hover:text-blue-500">Contacts</a>
+        <a href="/" className="hover:text-blue-500">Trang chủ</a>
+        <a href="IT" className="hover:text-blue-500">Dịch vụ IT</a>
+        <a href="web_development" className="hover:text-blue-500">Web Development</a>
+        <a href="mobile" className="hover:text-blue-500">Mobile Development</a>
+        <a href="contact" className="hover:text-blue-500">Liên hệ</a>
       </div>
       <div className="text-white text-center py-4">Copyright © 2022 Engitech by OceanThemes. All Rights Reserved.</div>
     </div>

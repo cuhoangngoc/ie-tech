b3a7aff5b76b8ed4e7311bacb1f32fdf801e9879
huygia0811 <20520203@gm.uit.edu.vn>
2022-12-09 23:21:09 +0700

    Add API add money and update page request of Admin

--- pages/admin/requests.js
+++ pages/admin/requests.js
@@ -2,20 +2,39 @@ import AdminLayout from "../../components/Admin/AdminLayout";
 import { useState, useEffect } from "react";
 
 export default function Admin() {
-  const [servicesData, setServicesData] = useState([]);
+  const [requestsData, setrequestsData] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       const res = await fetch("/api/getrequest");
       const data = await res.json();
-      setServicesData(data); // Lưu dữ liệu vào state
+      setrequestsData(data); // Lưu dữ liệu vào state
     }
-
     fetchData(); // Gọi hàm fetchData
   }, []);
 
   const Check = (e) => {
-    
+    e.preventDefault();
+    const test = e.target.getAttribute("data-test");
+    if (test == 1) {
+      alert("Bạn đã duyệt trước đó! Không thể tiếp tục duyệt!!!");
+    } else {
+      const id = e.target.getAttribute("data-id");
+      const postData = async () => {
+        const response = fetch("/api/addMoney", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            id: id,
+          }),
+        });
+      };
+      postData();
+      alert("Bạn vừa phê duyệt yêu cầu nạp tiền của khách hàng!");
+      location.reload()
+    }
   };
 
   return (
@@ -33,15 +52,24 @@ export default function Admin() {
           </tr>
         </thead>
         <tbody>
-          {servicesData.map((service) => (
-            <tr key={service.id}>
-              <td>{service.id}</td>
-              <td>{service.user_id}</td>
-              <td>{service.amount}</td>
-              <td>{service.date}</td>
-              <td>{service.account_number}</td>
-              <td>{service.test}</td>
-              <td><button onClick={Check}>Duyệt</button></td>
+          {requestsData.map((request) => (
+            <tr key={request.id}>
+              <td>{request.id}</td>
+              <td>{request.user_id}</td>
+              <td>{request.amount}</td>
+              <td>{request.date}</td>
+              <td>{request.account_number}</td>
+              <td>{request.test}</td>
+              <td>
+                <button
+                  className="rounded bg-green-300 p-2 hover:bg-green-700"
+                  onClick={Check}
+                  data-test={request.test}
+                  data-id={request.id}
+                >
+                  Duyệt
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
---
+++ pages/api/addMoney.js
@@ -0,0 +1,34 @@
+import mysql from "mysql2/promise";
+
+export default async function handler(req, res) {
+  const connection = await mysql.createConnection({
+    host: "localhost",
+    database: "ietech",
+    user: "root",
+    password: "",
+    port: "3306",
+  });
+
+  try {
+    const val = [req.body.id];
+    const query = "UPDATE payments SET test = 1 WHERE id = ?";
+    await connection.execute(query, val);
+
+    const query2 = "SELECT * FROM payments WHERE id = ?";
+    const [rows2] = await connection.execute(query2, val);
+    let money_add = rows2[0].amount;
+    let userid = rows2[0].user_id;
+    const value1 = [userid];
+
+    const query1 = "SELECT * FROM users WHERE id = ?";
+    const [rows] = await connection.execute(query1, value1);
+    let money = rows[0].balance;
+    money = money + money_add;
+
+    const query3 = "UPDATE users SET balance = ? WHERE id = ?";
+    const value2 = [money, userid]
+    await connection.execute(query3, value2);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+}

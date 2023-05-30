a6fec6d4f341ce38a3c1bdf1572a9779be7dbf9b
CipherTD <tien03dat@gmail.com>
2023-05-30 12:29:43 +0700

    Update introduction.js

--- pages/introduction.js
+++ pages/introduction.js
@@ -9,7 +9,10 @@ import Layout from "../components/Layout/Layout";
 export default function GioiThieu() {
   return (
     <Layout>
-
+      <Head>
+        <title>Giới thiệu</title>
+        <meta name="description" content="Giới thiệu" />
+      </Head>
       <AboutUs />
       <Stat />
       <Cards />

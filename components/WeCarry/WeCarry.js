be2e042c1cdbaadcef7fc0e5959a6c660c742b3c
CipherTD <tien03dat@gmail.com>
2023-05-30 12:23:27 +0700

    Update WeCarry.js

--- components/WeCarry/WeCarry.js
+++ components/WeCarry/WeCarry.js
@@ -1,32 +1,26 @@
 import Link from 'next/link';
 import Image from 'next/image';
-
-const WeCarry = () => {
+export const WeCarry = () => {
   return (
-    <div className="flex m-auto h-[350px] max-w-[1100px] flex-col md:flex-row">
-      <div className="relative w-full h-full flex-grow">
+    <div className="m-auto flex h-[350px] max-w-[1100px] flex-col md:flex-row">
+      <div className="relative w-full h-full">
         <Image
           src="/asset/contact.jpg"
           alt="background image"
-          layout="fill"
           objectFit="cover"
         />
       </div>
-      
-      <div className="pt-16 flex justify-center md:justify-start md:pl-20">
-        <h1 className="text-2xl font-bold text-blue-600 md:text-4xl">
-          Let's Build Your Website!
+      <div className="pt-16">
+      <h1 className="pl-4 pt-10 text-[30px] font-bold text-blue-600 md:pl-20 md:text-[50px]">
+          Let&apos;s Build Your Website!
         </h1>
-      </div>
         <Link
           href="/contact"
-          className="m-3 border-2 p-3 text-blue-600 hover:bg-blue-500 hover:text-white"
+          className="m-3 border-2 p-3 text-blue-600 hover:bg-blue-500 hover:text-white md:ml-28"
         >
           CONTACT US
         </Link>
+      </div>
     </div>
-    
   );
 };
-
-export default WeCarry;

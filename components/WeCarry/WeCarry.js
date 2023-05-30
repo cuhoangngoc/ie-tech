f18593791542b06b586fa5c7ea9059638892c5c0
CipherTD <tien03dat@gmail.com>
2023-05-30 09:11:13 +0700

    Update Wecarry

--- components/WeCarry/WeCarry.js
+++ components/WeCarry/WeCarry.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 export const WeCarry = () => {
   return (
-    <div className="m-auto flex h-[350px]  max-w-[1100px] flex-col bg-[url('../public/asset/contact.jpg')] md:flex-row">
+    <div className="m-auto flex h-[350px]  max-w-[1100px] flex-col bg-[url('../public/asset/contact.webp')] md:flex-row">
       <h1 className="pl-4 pt-10 text-[30px] font-bold text-blue-600 md:pl-20 md:text-[50px]">
         Let&apos;s Build Your Website!
       </h1>
